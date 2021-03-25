65a5b24a7cd29b92e184b65e14fcf1ac9f0a7845
MatzJailson <jailsonmatz@outlook.com>
2021-03-25 03:27:31 -0300

    Features added in middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,65 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
+  const {username} = request.headers;
+
+  const userExists = users.find( user => user.username === username);
+
+  if(!userExists){
+    return response.status(404).json({error: 'User not found!'})
+  }
+
+  request.user = userExists;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  const userTodos = user.todos.length;
+
+  if(!user.pro && userTodos >= 10){
+    return response.status(403).json({error: 'Exceeded the limit of the free version'})
+  }
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+
+  const userExists = users.find( user => user.username === username );
+
+  if(!userExists){
+    return response.status(404).json({error:'User not found!!'});
+  }
+
+  const isValidId = validate(id);
+  if(!isValidId) {
+    return response.status(400).json({error:'Invalid ID'});
+  }
+
+  const todoExists = userExists.todos.find( todo => todo.id === id );
+  if(!todoExists){
+    return response.status(404).json({error: 'Todo not found!'})
+  }
+
+  request.user = userExists;
+  request.todo = todoExists;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const userExists = users.find( user => user.id === id);
+
+  if(!userExists){
+    return response.status(404).json({error:'User not found!!'});
+  }
+  request.user = userExists;
+  return next();
 }
 
 app.post('/users', (request, response) => {
